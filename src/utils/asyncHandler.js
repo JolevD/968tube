62264ebb33429d795723c4afb8689130e6361829
Jolev <141236807+JolevD@users.noreply.github.com>
2025-01-08 22:53:38 +0530

    asyncHandler-1

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,29 @@
 // as we will talk to the DB again and again in different situations , we will have this wrapper/function whenever we want to execute a function inside or talk to the database 
 
 // this is one method which is also used in industries but we will be not using this in this project 
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next)
-    } catch (error) {
-        res.status(error.code || 500).json({
-            message: error.message,
-            success: false
-        })
-    }
 
-} 
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch(err => next(err.message))
+
+    }
+}
+
+export { asyncHandler }
+
+////////////////////////
+// need to recheck this file
+/////////////////////////
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             message: error.message,
+//             success: false
+//         })
+//     }
+
+// } 
