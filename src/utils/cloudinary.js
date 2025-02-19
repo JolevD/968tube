cfde3987fc48d9cb1562ba869238c634b2a70eb2
Jolev <141236807+JolevD@users.noreply.github.com>
2025-02-19 21:48:21 +0530

    cloudinary file upload func

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadFilesOnCloudinary = async function (localFilePath) {
+
+    // Upload a file
+    try {
+        if (!localFilePath) return null
+
+        const uploadResult = await cloudinary.uploader
+            .upload(
+                localFilePath, {
+                resource_type: 'auto',
+            }
+            )
+        console.log("file upload successfull!", uploadResult.url);
+        return uploadResult
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        console.log(error);
+        return null
+    }
+
+};
+
+export { uploadFilesOnCloudinary } 
