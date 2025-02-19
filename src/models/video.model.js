067e05a384a0e9f9a5b8ecead45836b5ea7d99a9
Jolev <141236807+JolevD@users.noreply.github.com>
2025-02-19 18:32:19 +0530

    user and video models

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,9 +1,10 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
         videoFile: {
-            type: String,
+            type: String, // cloudinary url
             required: true
         },
         thumbnail: {
@@ -39,5 +40,7 @@ const videoSchema = new Schema(
         timestamps: true
     }
 )
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("User", videoSchema) 
