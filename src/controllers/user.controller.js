46de9d0b810980b9279c369018fd6e651997b7ef
Jolev <141236807+JolevD@users.noreply.github.com>
2025-02-24 22:17:41 +0530

    user controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,59 @@
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/apiError.js"
+import { apiResponse } from "../utils/apiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { uploadFilesOnCloudinary } from "../utils/cloudinary.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "success"
+    const { username, password, email, fullname } = req.body
+
+    if ([username, password, email, fullname].some((field) => field?.trim() === "")) {  // revise .some method 
+        throw new ApiError(400, "All fields required")
+    }
+
+    const existingUser = User.findOne({
+        $or: [{ username }, { email }]  // $or is a mongoDB operator which can check for both conditions at the same time here
     })
+
+    if (existingUser) {
+        throw new ApiError(409, "user already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path // here req comes from the multer middleware which adds the files fields so we have to check if the file or spcifically the local avatar file path is available or not. same for the coverimage 
+
+    const coverImageLocalPath = req.files?.coverImage[0]?.path  // revise the index property here
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "avatar is required")
+    } // checking because the avatar field is required 
+
+
+    const avatar = await uploadFilesOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadFilesOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "avatar is required")
+    }
+
+    const user = await User.create({
+        username: username.toLowerCase(),
+        fullname,
+        password,
+        email,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    })
+
+    const createdUser = await User.findById(user._id)?.select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new ApiError(500, "Error while registering user")
+    }
+
+    return res.status(201).json(
+        new apiResponse(200, createdUser, "User created successfully")
+    )
+
 })
 
 export { registerUser }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser) // gets here from the app.js and the gets to the /register route from this
+router.route("/register").post(upload.fields([
+    {
+        name: "avatar",
+        maxCount: 1
+    },
+    {
+        name: "coverImage",
+        maxCount: 1
+    }
+]), registerUser) // gets here from the app.js and the gets to the /register route from this
 
 export default router
