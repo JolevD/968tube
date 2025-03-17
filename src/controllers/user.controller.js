87b2aaf110ac4cc6f24c9af1d578e9e0dcc550e5
Jolev <141236807+JolevD@users.noreply.github.com>
2025-03-17 23:24:13 +0530

    tweet model add

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -134,7 +134,7 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
-    User.findByIdAndUpdate(req.user._id,
+    await User.findByIdAndUpdate(req.user._id,
         {
             $set: {
                 refreshToken: undefined // removes refresh token 
